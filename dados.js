f97f2b49a11a861f3b8a420acb9fd457d39f8130
clinicatricomaster <clinicatricomaster@gmail.com>
2025-12-12 11:29:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,85 @@
+const siteConfig = {
+    // 1. INFORMAÇÕES BÁSICAS
+    whatsapp: "5511930731230", 
+    instagram: "@tricomaster",
+
+    // VÍDEO INSTITUCIONAL
+    videoInstitucional: "https://www.youtube.com/embed/SEU_VIDEO_AQUI", 
+
+    // 2. IMAGENS GERAIS
+    logo: "img/logo.png", 
+    bannerPrincipal: "img/banner-hero.jpg", 
+    quemSomos: "img/quem-somos.jpg",
+
+    // 3. FOTOS E TEXTOS DOS TRATAMENTOS
+    tratamento1: "img/calvicie-masculina.jpg",
+    desc_tratamento1: "A Alopecia Androgenética masculina é a causa mais comum de perda de cabelos. Ocorre devido à sensibilidade genética aos hormônios andrógenos, levando ao afinamento progressivo dos fios.",
+    
+    tratamento2: "img/calvicie-feminina.jpg",
+    desc_tratamento2: "Diferente dos homens, a calvície feminina costuma se apresentar como um afinamento difuso, principalmente no topo da cabeça, preservando a linha frontal.",
+
+    tratamento3: "img/efluvios.jpg",
+    desc_tratamento3: "O Eflúvio Telógeno é uma queda intensa e temporária, geralmente desencadeada por fatores como pós-parto, estresse agudo, pós-cirúrgico ou deficiências vitamínicas.",
+
+    tratamento4: "img/areata.jpg",
+    desc_tratamento4: "A Alopecia Areata é uma doença autoimune que provoca falhas circulares (em forma de moeda) no couro cabeludo ou barba, podendo evoluir para perda total.",
+
+    tratamento5: "img/fibrosante.jpg",
+    desc_tratamento5: "A Alopecia Frontal Fibrosante é uma forma de alopecia cicatricial que causa o recuo da linha de implantação dos cabelos, muitas vezes acompanhada da perda de sobrancelhas.",
+
+    tratamento6: "img/dermatite.jpg",
+    desc_tratamento6: "A Dermatite Seborreica é uma inflamação crônica que causa descamação (caspa), vermelhidão e coceira, podendo acelerar a queda de cabelo se não tratada.",
+
+    tratamento7: "img/tratamento7.jpg",
+    desc_tratamento7: "Protocolo exclusivo para fortalecimento da haste capilar, devolvendo brilho e espessura aos fios danificados por química.",
+
+    tratamento8: "img/tratamento8.jpg",
+    desc_tratamento8: "Terapia regenerativa focada na saúde do couro cabeludo, estimulando a circulação sanguínea e a oxigenação dos folículos.",
+
+    tratamento9: "img/tratamento9.jpg",
+    desc_tratamento9: "Abordagem integrativa que combina nutrição e tecnologia para tratar quedas multifatoriais e melhorar a qualidade geral dos fios.",
+
+    // 4. FOTOS DA ESTRUTURA
+    estrutura1: "img/recepcao.jpg",
+    estrutura2: "img/lounge.jpg",
+    estrutura3: "img/consultorio.jpg",
+    estrutura4: "img/tecnologia.jpg",
+    estrutura5: "img/cirurgico.jpg",
+    estrutura6: "img/recuperacao.jpg",
+    estrutura7: "img/estrutura7.jpg",
+    estrutura8: "img/estrutura8.jpg",
+    estrutura9: "img/estrutura9.jpg",
+    estrutura10: "img/estrutura10.jpg",
+
+    // 5. PORTFÓLIO DE CASOS (AGORA 14 CASOS)
+    caso1_antes: "img/caso1-antes.jpg",
+    caso1_depois: "img/caso1-depois.jpg",
+    caso2_antes: "img/caso2-antes.jpg",
+    caso2_depois: "img/caso2-depois.jpg",
+    caso3_antes: "img/caso3-antes.jpg",
+    caso3_depois: "img/caso3-depois.jpg",
+    caso4_antes: "img/caso4-antes.jpg",
+    caso4_depois: "img/caso4-depois.jpg",
+    caso5_antes: "img/caso5-antes.jpg",
+    caso5_depois: "img/caso5-depois.jpg",
+    caso6_antes: "img/caso6-antes.jpg",
+    caso6_depois: "img/caso6-depois.jpg",
+    caso7_antes: "img/caso7-antes.jpg",
+    caso7_depois: "img/caso7-depois.jpg",
+    caso8_antes: "img/caso8-antes.jpg",
+    caso8_depois: "img/caso8-depois.jpg",
+    caso9_antes: "img/caso9-antes.jpg",
+    caso9_depois: "img/caso9-depois.jpg",
+    caso10_antes: "img/caso10-antes.jpg",
+    caso10_depois: "img/caso10-depois.jpg",
+    
+    // NOVOS
+    caso11_antes: "img/caso11-antes.jpg",
+    caso11_depois: "img/caso11-depois.jpg",
+    caso12_antes: "img/caso12-antes.jpg",
+    caso12_depois: "img/caso12-depois.jpg",
+    caso13_antes: "img/caso13-antes.jpg",
+    caso13_depois: "img/caso13-depois.jpg",
+    caso14_antes: "img/caso14-antes.jpg",
+    caso14_depois: "img/caso14-depois.jpg"
+};
